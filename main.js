67bc9c280a67932eba3f759eb745a7ec5b506432
fluffywaffles <skorlir@gmail.com>
2016-11-05 22:39:13 -0500

    Move positioning into CSS so that resize doesn't cause drift.

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ var period = 30
 
 var pts = Array(periods*period*2|0).fill(0)
 
-var offset = (window.innerWidth - 150) >> 1
-
 for (i = 0; i <= periods*period|0; i++) {
   if (i % (period|0) == 0) amplitude = (Math.random()*20 + 20)
-  pts[2*i] = offset + i
-  pts[2*i + 1] = (window.innerHeight/2) + (amplitude * Math.cos(freq * (i + (period/2)|0)) + amplitude)
+  pts[2*i] = i
+  pts[2*i + 1] = (amplitude * Math.cos(freq * (i + (period/2)|0)) + amplitude)
 }
 
 var paper = Snap('#melt-svg')
