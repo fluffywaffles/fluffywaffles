a7aecef34f6e4c10c3bc0ee5dec5383888a2476e
fluffywaffles <skorlir@gmail.com>
2016-11-06 11:44:00 -0600

    Fix `develop` script and make syrup drips animate.

--- main.js
+++ main.js
@@ -5,17 +5,53 @@ var amplitude = 20
 
 var pts = Array(periods*period*2|0).fill(0)
 
-for (i = 0; i <= periods*period|0; i++) {
+for (i = 0; i < periods*period|0; i++) {
   if (i % (period|0) == 0) amplitude = (Math.random()*20 + 20)
   pts[2*i] = i
   pts[2*i + 1] = (amplitude * Math.cos(freq * (i + (period/2)|0)) + amplitude)
 }
 
+var paths = Array(periods)
+for (p = 0; p < periods; p++) {
+  // console.log(`
+  //   From ${2*(p*period|0)}
+  //   To   ${2*(p*period + period|0)}
+  // `)
+  paths[p] = pts.slice(2*(p*period|0), 2*(p*period + period|0))
+}
+
 var paper = Snap('#melt-svg')
+var drips = []
 
-var pts3 = pts.slice()
+for (let path of paths) {
+  var drip = paper.polyline(path)
+  drip.attr({
+    fill: '#ffd200'
+  })
+  drips.push(drip)
+}
 
-var drip = paper.polyline(pts3)
-drip.attr({
-  fill: '#ffd200'
-})
+drips[0].animate({
+  transform: randomScaleTransform()
+}, 1000)
+
+var drips_g = paper.group.apply(paper, drips)
+
+function randomScaleTransform () {
+  return 'scale(1 ' + ((Math.random()/2) + 0.5) + ')'
+}
+
+function dribble (drip, after) {
+  drip.animate({
+    transform: randomScaleTransform()
+  }, 1000, mina.easeinout, after)
+}
+
+function perpetualDribble (drip) {
+  function x () {
+    dribble(drip, x)
+  }
+  dribble(drip, x)
+}
+
+drips.map(perpetualDribble)
